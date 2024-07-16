5b0ea1090b1ffc835819f8c6c4dfebf8ac2d4752
Adriangr7 <132469321+Adriangr7@users.noreply.github.com>
2024-07-16 17:37:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Regalo lucia');
-    alert('Regalo para mi novia');
+    alert('¡FELIZ CUMPLEAÑOS!');
 });
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Datos de prueba (en un entorno real, verifica en el servidor)
+    const validUsername = 'lucia';
+    const validPassword = 'adri';
+
+    if (username === validUsername && password === validPassword) {
+        // Redirigir a la página de contenido
+        window.location.href = 'index.html';
+    } else {
+        // Mostrar mensaje de error
+        document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos';
+    }
+});
+
 
