bd7fd5015c0ddb6f4e8149c5dec4cb2ee2a9fadc
Adriangr7 <132469321+Adriangr7@users.noreply.github.com>
2024-07-25 19:53:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -22,4 +22,42 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     }
 });
 
+let currentSlide = 0;
+
+function nextVideo() {
+    const carousel = document.getElementById('videoCarousel');
+    const videos = Array.from(carousel.getElementsByTagName('video'));
+    let activeIndex = videos.findIndex(video => video.classList.contains('active'));
+    videos[activeIndex].pause(); // Añade esta línea
+    videos[activeIndex].classList.remove('active');
+    activeIndex = (activeIndex + 1) % videos.length;
+    videos[activeIndex].classList.add('active');
+    videos[activeIndex].play();
+}
+
+document.getElementById('quizForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var correctAnswers = 0;
+
+    var answer1 = document.getElementById('question1').value;
+    var answer2 = document.getElementById('question2').value;
+    var answer3 = document.getElementById('question3').value;
+    var answer4 = document.getElementById('question4').value;
+
+    if (answer1.toLowerCase() === 'hola') correctAnswers++;
+    if (answer2.toLowerCase() === 'lsan') correctAnswers++;
+    if (answer3.toLowerCase() === 'burguer') correctAnswers++;
+    if (answer4.toLowerCase() === 'luna') correctAnswers++;
+
+    alert('Respuestas correctas: ' + correctAnswers); // Depuración
+
+    if (correctAnswers === 4) {
+        window.location.href = 'password.html';
+    } else {
+        alert('Alguna respuesta es incorrecta. Intenta de nuevo.');
+    }
+});
+
+
 
